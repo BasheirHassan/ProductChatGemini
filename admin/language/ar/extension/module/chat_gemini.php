a8a5885a84d798643ca7e75798df1fc7c4694344
OFFices-PC <OFFices-PC@OFFICES-PC>
2024-10-15 15:44:15 +0300

    تغيير المسمى الى chat gemini

---
+++ admin/language/ar/extension/module/chat_gemini.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']               = ' Chat  Gemini';
+
+// Text
+$_['text_extension']              = 'Extensions';
+$_['text_success']                = 'تم تعديل بنجاح  Chat  Gemini module!';
+$_['text_edit']                   = 'تعديل  Chat  Gemini Module';
+
+// Entry
+$_['entry_status']                = 'حالة';
+$_['entry_api_key']               = 'Gemini API Key';
+$_['entry_content_result']        = 'Gemini Response';
+
+// Error
+$_['error_api_key']               = 'Gemini API Key مطلوب';
+$_['error_permission']            = 'لاتملك صلاحيات تعديل  Chat  Gemini!';
